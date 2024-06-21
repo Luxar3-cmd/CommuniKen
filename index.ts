8a95e37bd73d6e3c6e31399ae8558eb04209e5ea
Ignacio Morales <eduardo.moraless@usm.cl>
2024-06-21 12:47:16 -0400

    Testeos de post

--- index.ts
+++ index.ts
@@ -20,11 +20,34 @@ app.get('/posts', ({db}) => {
     return db.post.findMany()
 })
 
+//Fetch a single post by ID
+app.get('/posts/:id',({db,params}) => {
+    return db.post.findUnique({where: {id: Number(params.id)}})
+})
+
+//Fetch a single post by slug
+app.get('/posts/path/:path', ({db,params}) => {
+    return db.post.findUnique({where: {path: String(params.path)}})
+})
+
 //Create a post
 app.post('/posts',({db,body}) => {
     return db.post.create({
         data: body as Post
     })
+})
+
+//Update a post
+app.put('/post/:id', ({db, params, body}) => {
+    return db.post.update({
+        where: {id: Number(params.id)},
+        data: body as Post
+    })
+})
+
+// Delete an existing post
+app.delete('/posts/:id', ({db,params}) => {
+    return db.post.delete({where: {id: Number(params.id)}})
 })
 
 // App listening on specified port
